526579aa7c011d1e6e0d8ca9ae28f7ab47729a81
Akhil <akhilu5042@gmail.com>
2022-01-21 11:11:30 +0530

    Program.cs

---
+++ DataStructureProblems_StacksAndQueues/Program.cs
@@ -0,0 +1,38 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Collections;
+using System.Collections.Generic;
+using Queue = DataStructureProblems_StacksAndQueues.Queue;
+using Stack = DataStructureProblems_StacksAndQueues.Stack;
+
+Console.WriteLine("Hello, World!");
+
+
+bool flag = true;
+while (flag)
+{
+    Console.WriteLine("\nEnter the program number which is to be executed \n 1.Stack, \n 2.Queue, \n 3.Exit");
+    int option = Convert.ToInt32(Console.ReadLine());
+    switch (option)
+    {
+        case 1:
+            Stack stack = new Stack();
+            stack.Push(70);
+            stack.Push(30);
+            stack.Push(56);
+            stack.Display();
+
+            break;
+        case 2:
+            Queue queue = new Queue();
+            queue.Enqueue(70);
+            queue.Enqueue(30);
+            queue.Enqueue(56);
+            queue.Display();
+            break;
+        case 3:
+            flag = false;
+            break;
+        
+    }
+}
---
+++ DataStructureProblems_StacksAndQueues/Queue.cs
@@ -0,0 +1,52 @@
+﻿using MyLinkedListProblems;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructureProblems_StacksAndQueues
+{
+   
+    public class Queue
+    {
+        Node front;
+        Node rear;
+        public Queue()
+        {
+            this.front = this.rear = null;
+        }
+        internal void Enqueue(int data)
+        {
+            Node newNode = new Node(data);
+            // If queue is empty, then new node is front and rear both 
+            if (this.rear == null)
+            {
+                this.front = this.rear = newNode;
+            }
+            else
+            {
+                // Add the new node at the end of queue and change rear
+                this.rear.next = newNode;
+                this.rear = newNode;
+            }
+            Console.WriteLine("{0} is inserted into Queue", data);
+        }
+        public void Display()
+        {
+            Node temp = this.front;
+            if (temp == null)
+            {
+                Console.WriteLine("Queue is empty");
+                return;
+            }
+            while (temp != null)
+            {
+                Console.Write(temp.data + " ");
+                temp = temp.next;
+            }
+            Console.WriteLine();
+        }
+    }
+}
